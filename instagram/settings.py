9c25805e905ea46f7f7d425354c32e869d3642b1
William Owalla <william.owalla@student.moringaschool.com>
2022-06-07 08:31:16 +0300

    register third party apps

--- instagram/settings.py
+++ instagram/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'instagram_app',
+    'crispy-forms',
 ]
 
 MIDDLEWARE = [
