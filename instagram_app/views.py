7d0e1becde88dc46c362ead609ab46311484ba10
William Owalla <william.owalla@student.moringaschool.com>
2022-06-07 20:18:33 +0300

    add login view logic

--- instagram_app/views.py
+++ instagram_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
+def login(request):
+    page = 'login'
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'username or password does not exist')
+    context = {'page': page}
+    return render(request, 'instagram_app/login-register.html', context)
