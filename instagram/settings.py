acd0fe20c2e61926cc80357791f07b69f75358d2
William Owalla <william.owalla@student.moringaschool.com>
2022-06-08 11:07:46 +0300

    set up email configurations

--- instagram/settings.py
+++ instagram/settings.py
@@ -155,3 +155,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_USE_TLS=True
+EMAIL_HOST=config('EMAIL_HOST')
+EMAIL_PORT=config('EMAIL_PORT')
+EMAIL_HOST_USER=config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
+
+
+django_heroku.settings(locals())
